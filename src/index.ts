5313cff6d0f4a8f2d426c8a866e4f29fdf602afd
Nicolás Antonelli <niconelli2@gmail.com>
2020-04-14 04:22:44 -0300

    Initial Structure, Modules & Configuration

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import {createConnection} from 'typeorm'
+
+import userRoutes from './routes/user.routes'
+
+const app = express();
+createConnection();
+
+// Middlewares
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes
+app.use(userRoutes);
+
+app.listen(3000);
+console.log("Server on Port", 3000);
